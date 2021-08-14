56ca86816d9ad9f3d373a5429fc63b31f097aa46
SevdaErkan <51799297+SevdaErkan@users.noreply.github.com>
2021-08-14 15:12:04 -0700

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,55 @@
+import { Component } from "react";
+import Chart from "./components/Chart";
+import "./styles.css";
+
+export default class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      chartData: {}
+    };
+  }
+  componentWillMount() {
+    this.getChartData();
+  }
+  getChartData = () => {
+    this.setState({
+      chartData: {
+        labels: [
+          "Boston",
+          "Worcester",
+          "Springfield",
+          "Lowell",
+          "Cambridge",
+          "New Bedford"
+        ],
+        datasets: [
+          {
+            label: "Population",
+            data: [617594, 181045, 153060, 106519, 105162, 95072],
+            backgroundColor: [
+              "rgba(255, 99, 132, 0.6)",
+              "rgba(54, 162, 235, 0.6)",
+              "rgba(255, 206, 86, 0.6)",
+              "rgba(75, 192, 192, 0.6)",
+              "rgba(153, 102, 255, 0.6)",
+              "rgba(255, 159, 64, 0.6)",
+              "rgba(255, 99, 132, 0.6)"
+            ]
+          }
+        ]
+      }
+    });
+  };
+  render() {
+    return (
+      <div className="App">
+        <Chart
+          chartData={this.state.chartData}
+          legendPosition="bottom"
+          location="Massachusetts"
+        />
+      </div>
+    );
+  }
+}
